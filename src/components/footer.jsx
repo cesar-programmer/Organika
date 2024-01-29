56752834b6a928b1d6fe01f748ec8912ec9016ae
Cesar <CesarCordova.developer@gmail.com>
2024-01-28 19:29:34 -0800

    adding a component counter

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+import './footer.css'
+export default function Footer() {
+  return (
+    <footer>
+      <p>Organika is a very expensive store that sell oranges</p>
+    </footer>
+  )
+}
---
+++ src/components/quantityPicker.jsx
@@ -0,0 +1,25 @@
+import './quantityPicker.css'
+import { useState } from 'react';
+
+export default function QuantityPicker() {
+  const [quantity, setQuantity] = useState(1);
+
+  function increase() {
+    setQuantity(quantity + 1);
+  }
+
+  function decrease() {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  }
+
+
+  return (
+    <div className="quantity-picker">
+      <button onClick={decrease}>-</button>
+      <label>{quantity}</label>
+      <button onClick={increase}>+</button>
+    </div>
+  )
+}
